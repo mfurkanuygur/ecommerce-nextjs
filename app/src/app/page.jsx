b51f6a1af52fe10d584b3d4d45343151d3822e87
M.Furkan Uygur <76523870+mfurkanuygur@users.noreply.github.com>
2024-04-29 15:46:11 +0300

    Update page.jsx

--- app/src/app/page.jsx
+++ app/src/app/page.jsx
@@ -15,7 +15,7 @@ export default async function Home() {
       <div className="flex flex-col gap-4">
         <div className="flex justify-between items-center ">
           <div className="flex flex-col gap-2 capitalize" >
-            <h6 className=" text-md font-semibold text-white">What's new</h6>
+            <h6 className=" text-md font-semibold text-white">Whats new</h6>
             <h3 className=" text-cyan-800 font-bold text-lg">take a look at some our new products</h3>
           </div>
           <button className=" border-2 border-cyan-800 py-2 px-4 rounded-xl capitalize transition text-cyan-800 hover:bg-cyan-800 hover:text-white">
